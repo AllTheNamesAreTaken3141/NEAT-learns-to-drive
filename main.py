3bb296785bce7ddc0b37a308e9a6cc7f927206b2
Joe's laptop <hey-there-joe@outlook.com>
2022-02-25 09:08:00 -0800

    implemented the most scuffed raycasting/distance detection this world has ever seen

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ WIN_HEIGHT = 500
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 CAR_IMG = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "carsprite.png")), (64, 64))
-TRACK_IMG = pygame.image.load(os.path.join("imgs", "track.png"))
+TRACK_IMG = pygame.image.load(os.path.join("imgs", "track.png"))\
 
-RAY_SURFACE = pygame.Surface((8,8))
-pygame.draw.circle(RAY_SURFACE, WHITE, (4,4), 4)
-RAY_COLLIDE_MASK = pygame.mask.from_surface(RAY_SURFACE)
+RAY_COLLIDE_SURFACE = pygame.Surface((8,8))
+pygame.draw.circle(RAY_COLLIDE_SURFACE, WHITE, (4,4), 4)
+RAY_COLLIDE_MASK = pygame.mask.from_surface(RAY_COLLIDE_SURFACE)
+
 
 class Track:
     IMG = TRACK_IMG
@@ -40,11 +42,9 @@ class Track:
         
         return False
 
-
-
 class Car:
     IMG = CAR_IMG
-    VEL = 9
+    VEL = 5
     ROT_VEL = 8
 
     def __init__(self, x, y, angle):
@@ -53,8 +53,7 @@ class Car:
         self.velocity.rotate_ip(angle)
         self.angle = -angle
         self.mask = pygame.mask.from_surface(CAR_IMG)
-        self.rays = [[-90,0]]
-        self.distances = [0]
+        self.rays = [[0, 0], [-45, 0], [-90, 0], [-135, 0], [-180, 0]]
 
     def draw(self, win):
         rotated_image = pygame.transform.rotate(CAR_IMG, self.angle)
@@ -63,34 +62,45 @@ class Car:
         win.blit(rotated_image, new_rect.topleft)
 
         for ray in self.rays:
-            pygame.draw.line(win, RED, (self.position.x, self.position.y), (self.position.x + sin(radians(ray[0] + self.angle)) * ray[1], self.position.y + cos(radians(ray[0] + self.angle)) * ray[1]))
+            pygame.draw.line(win, GREEN, (self.position.x, self.position.y), (self.position.x + sin(radians(ray[0] + self.angle)) * ray[1], self.position.y + cos(radians(ray[0] + self.angle)) * ray[1]))
 
     def move(self):
-        pass
-        #self.position = self.position + self.velocity
+        self.position = self.position + self.velocity
 
     def turn(self, strength):
         """strength is between -1 and 1 and determines the amount the car turns. Negative values turn _ and positive values turn _"""
         self.velocity.rotate_ip(self.ROT_VEL * strength)
         self.angle -= self.ROT_VEL * strength
 
+    def raycast(self, track):
+        for r in range(len(self.rays)):
+            for i in range(0, 1000, 5):
+                if self.collide_ray(self.rays[r][0] + self.angle, i, track):
+                    self.rays[r][1] = i
+                    break
+
     def collide_ray(self, angle, distance, track):
-        position = (sin(radians(angle)) * distance + self.position.x, cos(radians(angle)) * distance + self.position.y)
+        position = ((sin(radians(angle)) * distance) + self.position.x, (cos(radians(angle)) * distance) + self.position.y)
 
         ray_rect = RAY_COLLIDE_MASK.get_rect(center = (position[0], position[1]))
         track_rect = track.MASK.get_rect(center = (400, 250))
 
-        
+        offset_x = track_rect.x - ray_rect.x
+        offset_y = track_rect.y - ray_rect.y
 
-    def raycast(self):
-        for r in range(len(self.rays)):
-            self.rays[r][1] = 50
-            
+        if RAY_COLLIDE_MASK.overlap(track.MASK, (offset_x, offset_y)):
+            return True
 
+        return False
 
+    def get_distances(self):
+        distances = []
+        for i in self.rays:
+            distances.append(i[1])
+        return distances
 
 def draw_window(win, track, car):
-    win.fill(WHITE)    
+    win.fill(WHITE)
 
     track.draw(win)
     car.draw(win)
@@ -118,8 +128,8 @@ def main():
         elif keys[pygame.K_RIGHT]:
             car.turn(1)
 
-        car.raycast()
-        car.move()
+        car.raycast(track)
+        #car.move()
 
         draw_window(win, track, car)
 
